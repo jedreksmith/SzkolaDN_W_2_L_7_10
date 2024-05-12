3554440b7848bff0c588cccafd30fdb6338c7bb9
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 22:31:46 +0200

    Add project files.

---
+++ Ex_w_2_l_7_10/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace W_2_l_7_10;
+using System.Net;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Budujemy trójkąt");
+
+            Console.WriteLine("Podaj pierwszy bok");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugi bok");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj trzeci bok");
+            int c = int.Parse(Console.ReadLine());
+
+            if (a >= b+c || b >= a+c || c >= a+b )
+            {
+                Console.WriteLine("Z tych boków nie zbudujesz trójkąta");
+            }
+            else { Console.WriteLine("Można zbudować trójkąt"); };
+
+        }
+    }
+
